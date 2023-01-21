83b6a40e59a08730ddd0d96bececd979762ba0f6
stainsleto <stainsmault@gmail.com>
2023-01-21 23:42:36 +0530

    @Stains/ Vite and Tailwind Configuration

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "dark-blue" : "#000235",
+        "primary" : "#FFFFFF",
+        "blue" : "#1157A2",
+      }
+    },
+  },
+  plugins: [],
+}
