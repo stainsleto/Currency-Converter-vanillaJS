45f390f8e7e70d679db39fe43c5e4e323db88139
stainsleto <stainsmault@gmail.com>
2023-01-23 22:48:17 +0530

    @Stains/Converter api Integration

--- src/main.js
+++ src/main.js
@@ -1,2 +1,39 @@
 import './style.css'
 
+document.addEventListener("DOMContentLoaded",()=>{
+  const convertButton = document.getElementById("convert");
+  convertButton.addEventListener('click',() => {
+    const amount = document.getElementById("amount");
+    const from = document.getElementById("form");
+    const to = document.getElementById("to");
+    const result = document.getElementById("result");
+
+
+    
+      let myHeaders = new Headers();
+      myHeaders.append("apikey", "YFsMiMLODqQlhsTSAaRIrYSdusufryk0");
+      
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+      };
+      
+      const convertApi = fetch(`https://api.apilayer.com/currency_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
+      const apiResponse = convertApi.then(response => response.json());
+      const data = apiResponse.then(daata =>{
+       console.log(daata.result);
+
+      })
+      
+    
+
+
+  });
+  
+
+  
+    
+
+})
+
